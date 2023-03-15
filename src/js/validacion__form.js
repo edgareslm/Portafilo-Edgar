fb5571ac1f550da3c224aeec4774396993426abe
Edgar Esquivel <edgarmel95@gmail.com>
2023-03-14 22:47:03 -0600

    Se incluye funcion para activar boton y validar una expresion regular al textarea

--- src/js/validacion__form.js
+++ src/js/validacion__form.js
@@ -1,30 +1,51 @@
 window.onload= function(){
-    const element_inputs = document.querySelectorAll(".contact__input");
+    const element_inputs = document.querySelectorAll('.contact__input');
+
+    const contact_button = document.querySelector('[data-type="submit"]');
+
+    function activateButton(){
+        const active__button = (()=> [...element_inputs].every(input => input.checkValidity() === true))(); // verifica si los campos son validos;
+        if(!active__button){
+            contact_button.setAttribute('disabled','');
+        }else{
+            contact_button.removeAttribute('disabled');
+        }
+    }
+
+    activateButton(); // valida si activar boton o no
 
     element_inputs.forEach(
         (input)=>{
-            input.addEventListener("blur",(event)=>{SelectionInput(event.target)})
+            input.addEventListener("blur",(event)=>{SelectionInput(event.target)});
         }
     )
 
     function SelectionInput(input){
         let type_input = input.dataset?.type;
         let input_parent = input.parentNode;
-        let input_alert = input_parent.querySelector(".contact__alert");
+        let input_alert = input_parent.querySelector('.contact__alert');
+
+        if(custom_validation[type_input]){
+            custom_validation[type_input](input);
+        }
+
         if(input.validity.valid){
-            input.classList.remove("contact__input--invalid");
+            input.classList.remove('contact__input--invalid');
             input_alert.innerHTML=showErrorMessage(input,type_input);
-            input_alert.classList.remove("contact__alert--active");
+            input_alert.classList.remove('contact__alert--active');
+            activateButton();
         }else{
-            input.classList.add("contact__input--invalid");
-            input.parentElement.querySelector(".contact__alert").innerHTML=showErrorMessage(input,type_input);
-            input_alert.classList.add("contact__alert--active");
+            input.classList.add('contact__input--invalid');
+            input.parentElement.querySelector('.contact__alert').innerHTML=showErrorMessage(input,type_input);
+            input_alert.classList.add('contact__alert--active');
+            activateButton();
         }
     }
 
     const messagesError = {
         name:{
             valueMissing: "El campo Nombre no puede estar vacio",
+            patternMismatch:"Se admiten solo letras entre 3 y 50 caracteres máximo"
         },
         email:{
             valueMissing: "El campo Correo no puede estar vacio",
@@ -32,6 +53,11 @@ window.onload= function(){
         },
         subject:{
             valueMissing: "El campo Asunto no puede estar vacio",
+            patternMismatch:"Se permiten de 3 a 50 caracteres maximo"
+        },
+        message:{
+            valueMissing: "Escribe un breve mensaje",
+            customError:"Se permiten de 3 a 300 caracteres maximo",
         },
     };
 
@@ -50,4 +76,14 @@ window.onload= function(){
         }
     }
 
+    const custom_validation = {
+        message: (input)=>{patternMatch(input)},
+    }
+
+    function patternMatch(input){
+        let pattern = /^(?=\w[a-zA-Z]).{5,20}$/ig;
+        let input_value = input.value;
+        let message_error = pattern.test(input_value)?"":"Se adminten maximo 300 caracteres"; 
+        input.setCustomValidity(message_error);
+    }
 }
